8b2b55cb7f60576de7c8c24eef9e15932d8eb6fd
haibozhang <1213768756@qq.com>
2019-01-06 17:00:21 +0800

    微信小程序学习

---
+++ nideshop-mini-program/app.js
@@ -0,0 +1,18 @@
+App({
+  onLaunch: function () {
+    try {
+      this.globalData.userInfo = JSON.parse(wx.getStorageSync('userInfo'));
+      this.globalData.token = wx.getStorageSync('token');
+    } catch (e) {
+      console.log(e);
+    }
+  },
+
+  globalData: {
+    userInfo: {
+      nickname: '点击登录',
+      avatar: ''
+    },
+    token: '',
+  }
+})
---
+++ nideshop/src/common/config/config.js
@@ -0,0 +1,17 @@
+// default config
+module.exports = {
+  default_module: 'api',
+  weixin: {
+    appid: 'wxa6083fde9580de7b', // 小程序 appid
+    secret: 'b95fa968f3f55e175c2d1fe80f728061', // 小程序密钥
+    mch_id: '', // 商户帐号ID
+    partner_key: '', // 微信支付密钥
+    notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
+  },
+  express: {
+    // 快递物流信息查询使用的是快递鸟接口，申请地址：http://www.kdniao.com/
+    appid: '', // 对应快递鸟用户后台 用户ID
+    appkey: '', // 对应快递鸟用户后台 API key
+    request_url: 'http://api.kdniao.cc/Ebusiness/EbusinessOrderHandle.aspx'
+  }
+};
